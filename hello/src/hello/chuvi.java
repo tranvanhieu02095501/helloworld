22d4f86239252483f5fd5b88626fc3b545af052e
tranvanhieu02095501 <tranvanhieu02095501@gmail.com>
2021-09-12 10:56:13 +0700

    chuvi

---
+++ hello/src/hello/chuvi.java
@@ -0,0 +1,13 @@
+package hello;
+
+
+import java.util.Scanner;
+
+public class chuvi {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double r = sc.nextDouble();
+		double pi = 3.14;
+		System.out.println("Circumference = " + (2 * pi * r));
+	}
+}
