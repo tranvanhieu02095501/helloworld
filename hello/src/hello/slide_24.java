aa132be6e085082a6ae97a881de44515ecb8e832
tranvanhieu02095501 <tranvanhieu02095501@gmail.com>
2021-09-15 16:42:56 +0700

    fix

--- hello/src/hello/slide_24.java
+++ hello/src/hello/slide_24.java
@@ -10,14 +10,24 @@ public class slide_24 {
 		
 		System.out.println("Enter to nember 2 : ");
 		int number2 = sc.nextInt();
-		 
+		
+		System.out.println("Enter to result : ");
+		int result = sc.nextInt();
+		
+		
 		if(number1 >= number2) {
-			System.out.println("number1 - number2 = " +( number1 - number2));
+			int c = number1 - number2;
+			if(c == result) {
+				System.out.println(" kq đúng");
+			}else {
+				System.out.println(" kq sai");
+			}
 			
 		}else if(number2 >= number1) {
-			System.out.println("number2 - number1 = " +( number2 - number1));
+			System.out.println(" nhap lai" );
 			
 		}
+		
 		
 	}
 }
