5003b264e724410c8d8b8cf81bb4fcdbebf00a31
tranvanhieu02095501 <tranvanhieu02095501@gmail.com>
2021-09-15 15:11:13 +0700

    FIX

--- hello/src/hello/silde_26.java
+++ hello/src/hello/silde_26.java
@@ -6,32 +6,29 @@ public class silde_26 {
 	
      public static void main(String[] args) {
     	 
-    	 final int x=1;
-    	 final int y=2;
-    	 final int z=3;
-    	 final int a=4;
-    	 final int b=5;
+    	 final int Single=1;
+    	 final int Er=2;
+    	 final int Mfs=3;
+    	 final int Hoh=4;
+    	 
 		Scanner sc = new Scanner (System.in);
 		System.out.println("Enter to status : ");
 		int status = sc.nextInt();
 		
-		if(status == x) {
+		if(status == Single) {
 		System.out.println(" rate 10 % & $0 -&8,375");
-		}else if (status == y)
+		}else if (status == Er)
 		{
 			System.out.println(" rate 25 % & $0 -&8,375");
 			}
-		else if (status == z)
+		else if (status == Mfs)
 		{
 			System.out.println(" rate 28 % & $0 -&8,375");
 			}
-		else if (status == a)
+		else if (status == Hoh)
 		{
 			System.out.println(" rate 33 % & $0 -&8,375");
 			}
-		else if (status == b)
-		{
-			System.out.println(" rate 35 % & $0 -&8,375");
-			}
+	
 	}
 }
