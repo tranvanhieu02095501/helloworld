c966a9d6692e32ee6eaaffe3e3502d1c6bc2b565
tranvanhieu02095501 <tranvanhieu02095501@gmail.com>
2021-09-15 16:50:04 +0700

    fix

--- hello/src/hello/slide_25.java
+++ hello/src/hello/slide_25.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class slide_25 {
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter to  age : ");
+		int age = sc.nextInt();
 		System.out.println("Enter to  weight : ");
 		float weight = sc.nextFloat();
 		System.out.println("Enter to  height : ");
